578fe5e1b0085ab7d419e674202e9c38d4ca938c
Mohammad Umme Ammara <ammaramohammad6@gmail.com>
2026-06-22 10:49:25 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -134,4 +135,7 @@ def delete_card(id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
